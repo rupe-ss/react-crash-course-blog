6142cce8aefe084eb8ba98a0fd29222daaf5ee2e
rupesss-09 <rupesss09@gmail.com>
2021-12-19 09:45:21 -0600

    Added footer css and items in footer component

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,5 +1,6 @@
 import Header from 'components/Header';
 import Nav from 'components/Nav';
+import Footer from 'components/Footer';
 
 const Layout = ({ children, search, setSearch }) => {
     return (
@@ -7,6 +8,7 @@ const Layout = ({ children, search, setSearch }) => {
             <Header title={'ReactBlog'} />
             <Nav search={search} setSearch={setSearch} />
             {children}
+            <Footer />
         </>
     );
 };
