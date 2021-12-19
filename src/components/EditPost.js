ea89f9ad4a453f1f2b71c4ab48d4b9f5ce0f3ff8
rupesss-09 <rupesss09@gmail.com>
2021-12-19 14:53:53 -0600

    Handling EditPost component when there is no post available

--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 const EditPost = ({
     posts,
@@ -10,6 +10,9 @@ const EditPost = ({
 }) => {
     const { id } = useParams();
     const post = posts.filter((post) => post.id.toString() === id);
+    console.log(post);
+    console.log(editTitle, editBody);
+
     useEffect(
         () => ({
             if(post) {
@@ -19,26 +22,41 @@ const EditPost = ({
         }),
         [post, setEditTitle, setEditBody]
     );
+
     return (
         <main className='NewPost'>
-            <form className='newPostForm'>
-                <label htmlFor='postTitle'>Title:</label>
-                <input
-                    id='postTitle'
-                    type='text'
-                    required
-                    value={editTitle}
-                    onChange={(e) => setEditTitle(e.target.value)}
-                />
-                <label htmlFor='postBody'>Post:</label>
-                <textarea
-                    id='postBody'
-                    required
-                    value={editBody}
-                    onChange={(e) => setEditBody(e.target.value)}
-                />
-                <button type='submit'>Submit</button>
-            </form>
+            {editTitle && (
+                <>
+                    <h2>Edit Post</h2>
+                    <form className='newPostForm'>
+                        <label htmlFor='postTitle'>Title:</label>
+                        <input
+                            id='postTitle'
+                            type='text'
+                            required
+                            value={editTitle}
+                            onChange={(e) => setEditTitle(e.target.value)}
+                        />
+                        <label htmlFor='postBody'>Post:</label>
+                        <textarea
+                            id='postBody'
+                            required
+                            value={editBody}
+                            onChange={(e) => setEditBody(e.target.value)}
+                        />
+                        <button type='submit'>Submit</button>
+                    </form>
+                </>
+            )}
+            {!editTitle && (
+                <>
+                    <h2>Post Not Found</h2>
+                    <p>Well, that's disappointing.</p>
+                    <p>
+                        <Link to='/'>Visit Our Homepage</Link>
+                    </p>
+                </>
+            )}
         </main>
     );
 };
