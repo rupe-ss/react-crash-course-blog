7e7b291480c9b256342e7d0e89907880ecbf59ce
rupesss-09 <rupesss09@gmail.com>
2021-12-19 14:43:01 -0600

    Using useEffect to setEditTitle and setEditBody if post is there

--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-const EditPost = ({ editTitle, setEditTitle, editBody, setEditBody }) => {
+const EditPost = ({
+    posts,
+    editTitle,
+    setEditTitle,
+    editBody,
+    setEditBody,
+}) => {
+    const { id } = useParams();
+    const post = posts.filter((post) => post.id.toString() === id);
+    useEffect(
+        () => ({
+            if(post) {
+                setEditTitle(post.title);
+                setEditBody(post.body);
+            },
+        }),
+        [post, setEditTitle, setEditBody]
+    );
     return (
         <main className='NewPost'>
             <form className='newPostForm'>
