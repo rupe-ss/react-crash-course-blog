d118d2c1c209237b51f1180ddc91a078c97393a4
rupesss-09 <rupesss09@gmail.com>
2021-12-18 19:13:13 -0600

    Adding css for Home component and displaying posts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,21 @@
-const Home = () => {
-    return <div>Home</div>;
+//import components
+import Feed from 'components/Feed';
+
+const Home = ({posts}) => {
+    return (
+        <main>
+            {posts.length ? (
+                posts.map((post) => <div>{post.title}</div>)
+            ) : (
+                <p style={{marginTop: '2rem'}}>No posts to display.</p>
+            )}
+            {/* {posts.length ? (
+                <div> {posts} </div>
+            ) : (
+                <p style={{marginTop: '2rem'}}>No posts to display.</p>
+            )} */}
+        </main>
+    );
 };
 
 export default Home;
