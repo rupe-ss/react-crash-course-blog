d03e69ca6c7d027bd84728a7da5effafe2004851
rupesss-09 <rupesss09@gmail.com>
2021-12-18 22:20:58 -0600

    Adding form in NewPost.js component

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const NewPost = () => {
-    return <div>New Post</div>;
+    return (
+        <main>
+            <h2>New Post</h2>
+            <form>
+                <label htmlFor='postTitle'>Title:</label>
+                <input id='postTitle' type='text' required />
+                <label htmlFor='postBody'>Post:</label>
+                <textarea id='postBody' required />
+                <button type='submit'>Submit</button>
+            </form>
+        </main>
+    );
 };
 
 export default NewPost;
