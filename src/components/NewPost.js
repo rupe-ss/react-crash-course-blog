0f5a78afce8d70fc9ac67140ab997d7eb4c3608a
rupesss-09 <rupesss09@gmail.com>
2021-12-19 09:17:48 -0600

    More work on NewPost component to handle submit function

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,14 +1,31 @@
 import React from 'react';
 
-const NewPost = () => {
+const NewPost = ({
+    handelSubmit,
+    postTitle,
+    setPostTitle,
+    postBody,
+    setPostBody,
+}) => {
     return (
         <main className='NewPost'>
             <h2>New Post</h2>
-            <form className='newPostForm'>
+            <form className='newPostForm' onSubmit={handelSubmit}>
                 <label htmlFor='postTitle'>Title:</label>
-                <input id='postTitle' type='text' required />
+                <input
+                    id='postTitle'
+                    type='text'
+                    required
+                    value={postTitle}
+                    onChange={(e) => setPostTitle(e.target.value)}
+                />
                 <label htmlFor='postBody'>Post:</label>
-                <textarea id='postBody' required />
+                <textarea
+                    id='postBody'
+                    required
+                    value={postBody}
+                    onChange={(e) => setPostBody(e.target.value)}
+                />
                 <button type='submit'>Submit</button>
             </form>
         </main>
