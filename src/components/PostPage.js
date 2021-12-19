4196b8e4457145d53613fa16c2fa0a2217880574
rupesss-09 <rupesss09@gmail.com>
2021-12-18 20:18:12 -0600

    Adding more code to handle no post in PostPage component

--- src/components/PostPage.js
+++ src/components/PostPage.js
@@ -4,18 +4,28 @@ import {useParams, Link} from 'react-router-dom';
 const PostPage = ({posts, handleDelete}) => {
     const {id} = useParams();
     const post = posts.find((post) => post.id.toString() === id);
-    console.log(id);
     return (
         <main className='PostPage'>
             <article className='post'>
-                <>
-                    <h2>{post.title}</h2>
-                    <p className='postDate'>{post.datetime}</p>
-                    <p className='postBody'>{post.body}</p>
-                    <button onClick={() => handleDelete(post.id)}>
-                        Delete Post
-                    </button>
-                </>
+                {post && (
+                    <>
+                        <h2>{post.title}</h2>
+                        <p className='postDate'>{post.datetime}</p>
+                        <p className='postBody'>{post.body}</p>
+                        <button onClick={() => handleDelete(post.id)}>
+                            Delete Post
+                        </button>
+                    </>
+                )}
+                {!post && (
+                    <>
+                        <h2>Post Not Found</h2>
+                        <p>Well, that's disappointing.</p>
+                        <p>
+                            <Link to='/'>Visit Our Homepage</Link>
+                        </p>
+                    </>
+                )}
             </article>
         </main>
     );
