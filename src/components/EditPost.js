5d6abf92d9070a3d1b0e56b6f2a30cf93f20da4e
rupesss-09 <rupesss09@gmail.com>
2021-12-19 14:36:22 -0600

    Added edit form in EditPost.js component

--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
-const EditPost = () => {
-    return <div>Edit Post</div>;
+const EditPost = ({ editTitle, setEditTitle, editBody, setEditBody }) => {
+    return (
+        <main className='NewPost'>
+            <form className='newPostForm'>
+                <label htmlFor='postTitle'>Title:</label>
+                <input
+                    id='postTitle'
+                    type='text'
+                    required
+                    value={editTitle}
+                    onChange={(e) => setEditTitle(e.target.value)}
+                />
+                <label htmlFor='postBody'>Post:</label>
+                <textarea
+                    id='postBody'
+                    required
+                    value={editBody}
+                    onChange={(e) => setEditBody(e.target.value)}
+                />
+                <button type='submit'>Submit</button>
+            </form>
+        </main>
+    );
 };
 
 export default EditPost;
