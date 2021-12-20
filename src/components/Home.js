3e4c9cea7d2f84190c1ffcf2c719bbb4796f57de
rupesss-09 <rupesss09@gmail.com>
2021-12-20 10:42:20 -0600

    Added isLoading and network error when one of these is true in Home.js component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,28 @@
 //import components
 import Feed from 'components/Feed';
 
-const Home = ({posts}) => {
+const Home = ({ posts, fetchError, isLoading }) => {
     return (
         <main className='Home'>
-            {posts.length ? (
+            {isLoading && <p className='statusMsg'> Loading posts...</p>}
+            {!isLoading && fetchError && (
+                <p className='statusMsg' style={{ color: 'red' }}>
+                    {fetchError}
+                </p>
+            )}
+            {!isLoading &&
+                !fetchError &&
+                (posts.length ? (
+                    <Feed posts={posts} />
+                ) : (
+                    <p style={{ marginTop: '2rem' }}>No posts to display.</p>
+                ))}
+            {/* Writing again when new props */}
+            {/* {posts.length ? (
                 <Feed posts={posts} />
             ) : (
-                <p style={{marginTop: '2rem'}}>No posts to display.</p>
-            )}
+                <p style={{ marginTop: '2rem' }}>No posts to display.</p>
+            )} */}
         </main>
     );
 };
